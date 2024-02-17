0f45903cb2b3adaf24aa3b5f0801b11576a14aa7
Nikhil <nk9599094257@gmail.com>
2024-02-17 14:25:44 +0530

    stingUP

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const authRoutes = require('./routes/Authentication Routes/authenticationRoutes')
-
-require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const authRoutes = require("./routes/Authentication Routes/authenticationRoutes");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
-app.use('/auth',authRoutes)
-// app.use()
+app.use(morgan("dev"));
+app.use("/v1/auth", authRoutes);
 
-module.exports = app
+module.exports = app;
---
+++ server/src/database/database.js
@@ -0,0 +1,17 @@
+const Pool = require("pg").Pool;
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+
+    const pool = new Pool({
+        user: process.env.USER,
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD,
+        port: process.env.DB_PORT,
+      });    
+
+
+
+module.exports = {pool}
