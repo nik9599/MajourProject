8f878c2cd426a8c791d485b2febcf6cbaac50d1a
Nikhil <nk9599094257@gmail.com>
2024-03-10 00:29:46 +0530

    updated

--- admin/src/Component/Pages/Card/PaymentCard/PaymentCard.jsx
+++ admin/src/Component/Pages/Card/PaymentCard/PaymentCard.jsx
@@ -2,18 +2,32 @@ import React from "react";
 import "./paymentCard.css";
 
 export default function PaymentCard({
-  product_name = "Noting",
-  product_quantity =0,
-  product_price =0,
+  product_name = "Noodles",
+  product_quantity = 10,
+  product_price = 110,
 }) {
   const total = product_price * product_quantity;
   return (
-    <div className="product-container" >
-      <p>{product_name}</p>
-      <p> {product_quantity} </p>
-      <p> {product_price} </p>
-      <p> {total} </p>
-      <div className="saperator" ></div>
+    <div className="product-container">
+      <div style={{display:"flex" ,width :"100%" , justifyContent:"space-evenly"}} >
+        <div className="name-div-card">
+          {" "}
+          <p>{product_name}</p>
+        </div>
+        <div className="quantity-div-card">
+          {" "}
+          <p> {product_quantity} </p>
+        </div>
+        <div className="price-div-card">
+          {" "}
+          <p> {product_price} </p>
+        </div>
+        <div className="total-div-card">
+          {" "}
+          <p> {total} </p>
+        </div>
+      </div>
+      <div className="saperator"></div>
     </div>
   );
 }
--- admin/src/Component/Pages/Payment/Payment.jsx
+++ admin/src/Component/Pages/Payment/Payment.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import "./payment.css";
+import PaymentCard from "../Card/PaymentCard/PaymentCard";
 
 export default function Payment() {
   return (
-    <div>
-      <div>
-        <p>Product Name</p>
-        <p>Product Quantity</p>
-        <p>Product Price</p>
-        <p>Total</p>
+    <div className="pay-container">
+      <div className="header-container">
+        <div className="name-div">
+          {" "}
+          <p>Product Name</p>
+        </div>
+        <div className="quantity-div">
+          {" "}
+          <p>Product Quantity</p>
+        </div>
+        <div className="price-div">
+          {" "}
+          <p>Product Price</p>
+        </div>
+        <div className="total-div">
+          {" "}
+          <p>Total</p>
+        </div>
       </div>
+      <div className="card-div" >
+        <PaymentCard />
+      </div>
+      <div></div>
     </div>
   );
 }
